0cab2e996ff55409d07e90101c5be6de677c9f69
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-03-28 09:30:52 +0300

    Add JS modules.

---
+++ modules/time.js
@@ -0,0 +1,7 @@
+export const { DateTime } = luxon;
+export const updateTime = () => {
+  const now = DateTime.now();
+  const dateTime = now.toLocaleString(DateTime.DATETIME_FULL);
+  document.getElementById('current-time').querySelector('p').textContent =
+    dateTime;
+};
